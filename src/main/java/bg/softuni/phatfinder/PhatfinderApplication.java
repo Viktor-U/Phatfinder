36c1acab1a40dfb018dc921b172535c6d41ba415
Viktor-U <viktor7gg@gmail.com>
2024-06-16 20:42:18 +0300

    up

---
+++ src/main/java/bg/softuni/phatfinder/PhatfinderApplication.java
@@ -0,0 +1,13 @@
+package bg.softuni.phatfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhatfinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhatfinderApplication.class, args);
+    }
+
+}
